482705b161617f38044577942d4ee80cd46c5757
Kendall Weistroffer <kendall.fonebone@gmail.com>
2017-12-06 11:32:22 -0700

    Setting up Q5

---
+++ src/Conversion.java
@@ -0,0 +1,25 @@
+public class Conversion {
+
+    /*
+        Convert a base-10 integer to an 8-bit two's complement binary number, and return
+        the binary number
+     */
+    public String base10to2(String base10){
+
+        return "";
+    }
+
+    /*
+        Return 2's complement negative off the given base10 number and return the
+        binary number
+     */
+
+    public String negBase2(String base10){
+
+        return "";
+    }
+
+
+    //Must be able to handle any base-10 integer that is submitted as a command line argument, even negative numbers
+    //Provide suitable error handling.
+}
