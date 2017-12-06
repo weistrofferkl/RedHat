9e347f42b184e22b438ca53ebe2bb5414a0fbcef
Kendall Weistroffer <kendall.fonebone@gmail.com>
2017-12-05 20:29:21 -0700

    Completed Q2

---
+++ src/Q2.java
@@ -0,0 +1,49 @@
+public class Q2 {
+    /*
+        Given that there are 50,000 items in the input String array, the
+        function must add all array items to outputString and then return
+        the outputString. If forceUpperCase is true, the return string mush be upper case
+     */
+
+
+    //Original Given Function:
+   /* public String addStringItems(String[] items, boolean forceUpperCase){
+        String returnValue = "";
+        for(int i = 0; i < items.length; i++){
+            returnValue += items[i];
+
+        }
+        return forceUpperCase?returnValue.toUpperCase():returnValue;
+    }
+   */
+
+    //Corrected Function:
+    public static String addStringItems(String[] items, boolean forceUpperCase){
+        //Java has a nifty thing called StringBuilder that makes stuff like this nice:
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i< items.length; i++){
+            //using the StringBuilder .append() to add the value:
+            sb.append(items[i]);
+        }
+
+        //Convert to string to return correct output format:
+        String output  = sb.toString();
+
+        //if forceUpperCase is true, we convert our output to Uppercase and return
+        //otherwise we just return the original output:
+        return forceUpperCase ? output.toUpperCase() : output;
+    }
+
+
+    public static void main(String[] args){
+        String[] s = {"Hi", "A", "PERER", "STUFF", "me", "o", "LOwerCaSe"};
+        String[] ns = s;
+
+        String newString = addStringItems(s, false);
+        String newString2 = addStringItems(ns, true);
+        System.out.println("NEW STRING: " + newString);
+        System.out.println("NEW STRING2: " + newString2);
+    }
+}
+
--- src/Range.java
+++ src/Range.java
@@ -1,2 +1,32 @@
 public interface Range {
+
+    /*
+        Create a new Range object representing an integer interval starting from "from" and "to", both
+        limits inclusive
+     */
+    public Range newRange(int from, int to);
+
+
+    /*
+        Return if "value" is in the range defined by this object
+     */
+    public boolean isIn(int value);
+
+    /*
+        Return the minimum value in range
+     */
+    public int min();
+
+    /*
+        Return the maximum value in range
+     */
+    public int max();
+
+    /*
+        Add range 'r' to this range and return 'this'.
+        'r' and this may denote disjoint ranges, for instace:
+        r.newRange(1,5).add(r.newRange(8,10)) denotes a range
+        including 1,2,3,4,5,8,9,10
+     */
+    public Range add(Range r);
 }
