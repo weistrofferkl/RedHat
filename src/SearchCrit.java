18ee5bd812d7f22732b1cdbc8c2bc3e60da19b47
Kendall Weistroffer <kendall.fonebone@gmail.com>
2017-12-07 15:58:04 -0700

    Adding in maximum number of fields check

--- src/SearchCrit.java
+++ src/SearchCrit.java
@@ -1,2 +1,36 @@
+import java.util.*;
 public class SearchCrit {
+
+    //Search Fields
+    private List<Fields> fieldsList;
+    private int numFieldsMax = 10; //Set Default Value
+
+    //Constructor:
+    public SearchCrit(int max){
+        this.fieldsList = new ArrayList<Fields>();
+        this.numFieldsMax = max;
+    }
+
+    //Getters and Setters:
+
+    public List<Fields> getFieldsList() {
+        return fieldsList;
+    }
+
+    public void setFieldsList(List<Fields> fieldsList) {
+        this.fieldsList = fieldsList;
+    }
+
+    public int getNumFieldsMax() {
+        return numFieldsMax;
+    }
+
+    public void setNumFieldsMax(int numFieldsMax) {
+        this.numFieldsMax = numFieldsMax;
+    }
 }
+
+
+
+
+
